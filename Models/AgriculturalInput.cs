8082bf3def310634efbf3d9188492c5a16247141
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-27 18:03:47 -0300

    Add agricultural input

---
+++ Models/AgriculturalInput.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ordermanager_dotnet.Models
+{
+    public class AgriculturalInput
+    {
+        [Required]
+        public int Id {get;set;}
+
+        [Required]
+        public string Name {get;set;}
+
+        [Required]
+        public string Sprayrate {get;set;}
+
+        [Required]
+        public float Price {get;set;}
+
+        [Required]
+        public int Quantity {get;set;}
+
+        [Required]
+        public int ProviderId {get;set;}
+        public Provider Providers {get;set;}
+    }
+}
