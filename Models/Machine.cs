17c22a70cbd03bc8a4f8f99535c217d7033c690e
Leandro Cunha <leandrocunha016@gmail.com>
2021-10-08 15:21:57 -0300

    Fix relationship machine with model machine

--- Models/Machine.cs
+++ Models/Machine.cs
@@ -6,23 +6,23 @@ namespace ordermanager_dotnet.Models
     public class Machine
     {
         [Required]
-        public int Id {get;set;}
+        public int Id { get; set; }
 
         [Required]
-        public string Type {get;set;}
+        public string Type { get; set; }
 
         [Required]
-        public int Year {get;set;}
+        public int Year { get; set; }
 
         [Required]
-        public string Implement{get;set;}
+        public string Implement { get; set; }
 
         [Required]
-        public DateTime AcquisitionDate {get;set;}
+        public DateTime AcquisitionDate { get; set; }
 
         [Required]
-        public int ModelId {get;set;}
+        public int ModelMachineId { get; set; }
 
-        public ModelMachine ModelsMachine {get;set;}
+        public ModelMachine ModelsMachine { get; set; }
     }
 }
