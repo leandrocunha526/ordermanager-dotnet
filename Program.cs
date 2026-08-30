cb45ae37c8d33a880382b783a2d1a1d463182641
Leandro Cunha <leandrocunhaemail@gmail.com>
2026-08-30 04:43:59 -0300

    Migration to .NET 10 and PostgreSQL (#5)
    * chore: Migration to .NET 10 and PostgreSQL
    
    Migration to .NET 10.
    Pomelo does not support .NET 10.
    I migrated to PostgreSQL using Npgsql with EF integration.
    
    * ci: Update .NET workflow for version and branches
    
    Updated the .NET workflow to use .NET 10.0.x and removed dotnet9-migration branch.
    
    * chore: Upgrade .NET SDK and ASP.NET Core runtime to 10.0 in Dockerfile
    
    * chore: Migrate API to PostgreSQL and add Adminer service (docker compose)
    
    Updated API service to use PostgreSQL and added Adminer for database management.
    
    * chore: Add migrations
    
    * chore: remove migrations from gitignore
    
    * fix: remove connection strings in appsettings.json
    
    * chore: Import needed changes fot docker compose file
    
    * chore: Add secret id in .csproj file
    
    * chore: Changes to migrate of the use of dotnet user secrets
    
    * fix: Remove AllowMissingPrunePackageData=true

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using ordermanager_dotnet.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using ordermanager_dotnet.Helpers;
@@ -10,10 +10,15 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-string connectionString = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<DataContext>(options => options.UseMySql(connectionString,
-ServerVersion.AutoDetect(connectionString)));
+string connectionString =
+    builder.Configuration.GetConnectionString("Default")
+    ?? throw new InvalidOperationException(
+        "ConnectionStrings:Default not found."
+    );
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseNpgsql(connectionString)
+);
 
 builder.Services.AddControllers().AddJsonOptions(x =>
   x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
@@ -49,7 +54,6 @@ builder.Services.AddAuthentication(x =>
             var user = userService.GetById(userId);
             if (user == null)
             {
-                // return unauthorized if user no longer exists
                 context.Fail("Unauthorized");
                 context.HttpContext.Response.StatusCode = 401;
                 // Should return a task with the failure result when the user is not found
@@ -84,9 +88,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend v1"));
 }
 
-app.UseRouting(); // Add this line to register EndpointRoutingMiddleware
+app.UseRouting();
 
-// Place the UseAuthorization and UseAuthentication before UseEndpoints
 app.UseAuthentication();
 app.UseAuthorization();
 
